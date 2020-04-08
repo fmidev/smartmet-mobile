02be9934bae20c40b74ea8aa74b9d4e939cad730
Ilja Leijo <ilja.leijo@fmi.fi>
2020-04-08 15:57:59 +0300

    Add placeholders for unit settings

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, FlatList } from 'react-native';
 import { HeaderBackButton } from 'react-navigation-stack';
 import { translate } from 'react-i18next';
 import i18n from 'i18next';
 import { ListItem } from 'react-native-elements';
 import AsyncStorage from '@react-native-community/async-storage';
-
+import RBSheet from "react-native-raw-bottom-sheet";
+const ITEMS = [...Array(5).keys()];
 const styles = StyleSheet.create({
   header: {
     paddingTop: 7,
@@ -19,6 +20,18 @@ const styles = StyleSheet.create({
     paddingLeft: 10,
     paddingBottom: 30,
     textAlign: 'center',
+  },
+  units: {
+    margin: 0.5,
+    paddingTop: 15,
+    paddingLeft: 18,
+    paddingBottom: 15,
+    backgroundColor: '#FFF',
+    width: '100%',
+    flex: 1,
+    alignSelf: 'center',
+    flexDirection: 'row',
+    borderRadius: 0,
   },
 });
 
@@ -35,6 +48,25 @@ export class SettingsScreen extends React.Component {
     } catch (error) {
       // TODO: Error handling
     }
+  }
+
+  renderItem({ item }) {
+    return (
+      <View style={styles.units} >
+        <TouchableOpacity onPress={() => this[RBSheet + item].open()}>
+          <Text>TESTITEM {item}</Text>
+        </TouchableOpacity>
+        <RBSheet
+          ref={ref => {
+            this[RBSheet + item] = ref;
+          }}
+        >
+          <View>
+            <Text>TESTITEM {item}</Text>
+          </View>
+        </RBSheet>
+      </View >
+    );
   }
 
   render() {
@@ -56,6 +88,14 @@ export class SettingsScreen extends React.Component {
             onPress={() => this.onChangeLang(currentLang)}
           />
         ))}
+        <Text style={styles.header}>
+          UNITS
+        </Text>
+        <FlatList
+          data={ITEMS}
+          renderItem={this.renderItem}
+          keyExtractor={(_, index) => index.toString()}
+        />
         <Text style={styles.header}>
           {t('settings:about')}
         </Text>
