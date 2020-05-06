2807ad07958ef9945c0e7b05a3c3f512f91f2e9b
Ilja Leijo <ilja.leijo@fmi.fi>
2020-05-06 10:52:15 +0300

    Add wms animation image prefetch before render

--- src/screens/MapsScreen.js
+++ src/screens/MapsScreen.js
@@ -1,132 +1,185 @@
 import Config from 'react-native-config';
-import React from 'react';
-import { StyleSheet, View, Dimensions } from 'react-native';
-import MapView, {
-  ProviderPropType,
-  WMSTile,
-} from 'react-native-maps';
+import React, { Component } from 'react';
 import { MapStyle } from "../MapStyle";
+import { StyleSheet, View, Text, Dimensions, Image, TouchableOpacity } from 'react-native';
+import MapView from 'react-native-maps';
+import moment from 'moment-with-locales-es6';
+import { LoadingView } from '../components';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const { width, height } = Dimensions.get('window');
 const ASPECT_RATIO = width / height;
 const LATITUDE = 41.662971
 const LONGITUDE = 74.749084
-const LATITUDE_DELTA = 13.5000;
+const LATITUDE_DELTA = 13.5000;;
 const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+const OVERLAY_TOP_LEFT_COORDINATE = [LATITUDE + (LATITUDE_DELTA / 2), LONGITUDE - (LATITUDE_DELTA / 2)];
+const OVERLAY_BOTTOM_RIGHT_COORDINATE = [LATITUDE - (LATITUDE_DELTA / 0.5), LONGITUDE + (LONGITUDE_DELTA / 0.5)];
+const wmsBaseUrl = `${Config.API_URL}/wms?bbox=8766409.902202941%2C5009377.08697311%2C1.001875417394622E7%2C6261721.358716387&CRS=EPSG%3A3067&format=image%2Fpng&height=1024&layers=mobile%3Agfs%3Aprecipitation&request=getmap&service=wms&styles=&transparent=true&version=1.3.0&width=1024`
+const wmsQueries = []
 
-const times = ['20200425T085000', '20200426T085010', '20200427T085020', '20200428T085030', '20200429T085040']
+export default class MapsScreen extends Component {
+  static propTypes = {
+    provider: MapView.ProviderPropType,
+  };
 
-class MapsScreen extends React.Component {
-  constructor(props, context) {
-    super(props, context);
+  constructor(props) {
+    super(props);
+
     this.state = {
+      loading: true,
+      wmsIndex: 0,
       region: {
         latitude: LATITUDE,
         longitude: LONGITUDE,
         latitudeDelta: LATITUDE_DELTA,
         longitudeDelta: LONGITUDE_DELTA,
       },
-      wms: {
-        apiUrl: Config.API_URL,
-        service: 'wms',
-        version: '1.3.0',
-        request: 'getmap',
-        format: 'image/png',
-        transparent: 'true',
-        layers: 'kalnur:gfs:temperature',
-        time: '20200424T085000',
-        wmsWidth: '{width}',
-        wmsHeight: '{height}',
-        crs: 'EPSG:3067',
-        styles: '',
-        bbox: '{minX},{minY},{maxX},{maxY}'
-      }
+      overlay: {
+        bounds: [OVERLAY_TOP_LEFT_COORDINATE, OVERLAY_BOTTOM_RIGHT_COORDINATE],
+      },
     };
+
   }
 
   componentDidMount() {
-    console.log('this.state', this.state)
 
-    times.forEach(element => {
-      let wms = { ...this.state.wms }
-      wms.time = element;
-      this.interval = setInterval(() => this.setState({ wms }), 1500);
-    });
+    this.getServerTime()
+      .then((serverTimeUtc) => {
+        const foobar = moment(serverTimeUtc)
+        const remainder = 5 - (foobar.minute() % 5);
+        const nextFiveFromServerTime = moment(foobar).add(remainder, 'minutes');
 
-    console.log('this.state updated', this.state)
+        for (let i = 0; i < Config.WMS_TIMESTEPS; i++) {
+          wmsQueries.push(wmsBaseUrl + '&time=' + nextFiveFromServerTime.add(10, 'minutes').utc().format('YYYYMMDDTHHmm'))
+        }
+
+        this.getWmsImages(wmsQueries).then(img => {
+          this.setState({ ...this.state, loading: false })
+          this.loopState()
+        })
+
+      })
+      .catch((err) => {
+        console.error(err.message); // TODO: Error handling
+      });
+
   }
 
-  componentWillUnmount() {
-    clearInterval(this.interval);
+  getServerTime = () => {
+    const clusterInfoUrl = `${Config.API_URL}/admin?what=clusterinfo`;
+    return fetch(clusterInfoUrl)
+      .then((response) => {
+        const serverTimeUtc = response.headers.get('Date');
+        return serverTimeUtc;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  getWmsImages = (wmsQueries) => {
+    return Promise.all(wmsQueries.map(img => Image.prefetch(img)))
+  }
+
+  loopState = async () => {
+    for (let i = 0; true; i++) {
+      await this.setLoopInterval()
+      this.setState({ ...this.state, wmsIndex: i % wmsQueries.length });
+    }
+  }
+
+  setLoopInterval = async () => {
+    return new Promise((resolve) => {
+      setTimeout(() => resolve(), 200)
+    })
   }
 
   render() {
-    const { region, wms } = this.state;
+    if (this.state.loading) {
+      return (
+        <LoadingView />
+      );
+    }
     return (
-      <View style={mapsStyles.container}>
+      <View style={styles.container}>
         <MapView
           provider={this.props.provider}
-          //mapType={MAP_TYPES.SATELLITE}
-          style={mapsStyles.map}
+          style={styles.map}
           customMapStyle={MapStyle}
-          initialRegion={region}
+          initialRegion={this.state.region}
         >
-          <WMSTile
-            urlTemplate={wms.apiUrl + '/wms?service=' + wms.service + '&version=' + wms.version + '&request=' + wms.request + '&format=' + wms.format + '&transparent=' + wms.transparent + '&layers=' + wms.layers + '&time=' + wms.time + '&width=' + wms.wmsWidth + '&height=' + wms.wmsHeight + '&CRS=' + wms.crs + '&styles=' + wms.styles + '&bbox=' + wms.bbox}
-            zIndex={1}
+          <MapView.Overlay
+            bounds={this.state.overlay.bounds}
+            image={wmsQueries[this.state.wmsIndex]}
             opacity={0.5}
-            tileSize={512}
-            epsgSpec={'EPSG:4326'}
           />
         </MapView>
-      </View>
+        <View style={styles.buttonContainer}>
+          <View style={styles.bubble}>
+            <Text style={styles.header}>Temperature</Text>
+            <Text style={styles.date}>{moment(wmsQueries[this.state.wmsIndex].substr(wmsQueries[this.state.wmsIndex].length - 13)).format('L')}</Text>
+            <Text style={styles.time}>{moment(wmsQueries[this.state.wmsIndex].substr(wmsQueries[this.state.wmsIndex].length - 13)).format('HH:mm:ss')}</Text>
+          </View>
+        </View>
+        <View style={styles.layerButtonContainer} >
+          <TouchableOpacity style={styles.layerButton} >
+            <Ionicons name="logo-buffer" size={40} style={{ color: 'black', paddingTop: 2, paddingBottom: 2, paddingLeft: 6, paddingRight: 6 }} />
+          </TouchableOpacity>
+        </View>
+      </View >
     );
   }
 }
 
-MapsScreen.propTypes = {
-  provider: ProviderPropType,
-};
-
-const mapsStyles = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
+    ...StyleSheet.absoluteFillObject,
     justifyContent: 'flex-end',
     alignItems: 'center',
+    position: 'relative',
+    height: '100%',
+    display: 'flex',
   },
   map: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
+    ...StyleSheet.absoluteFillObject,
   },
   bubble: {
-    flex: 1,
     backgroundColor: 'rgba(255,255,255,0.7)',
-    paddingHorizontal: 18,
-    paddingVertical: 12,
-    borderRadius: 20,
-  },
-  latlng: {
-    width: 200,
-    alignItems: 'stretch',
-  },
-  button: {
-    width: 80,
     paddingHorizontal: 12,
+    paddingVertical: 8,
+    borderRadius: 12,
+  },
+  header: {
+    fontWeight: 'bold',
+    paddingBottom: 5,
+    fontSize: 12,
+  },
+  date: {
+    fontSize: 10,
+    textAlign: 'center',
+  },
+  time: {
+    textAlign: 'center',
+    fontSize: 10,
     alignItems: 'center',
-    marginHorizontal: 10,
+    justifyContent: 'center',
+  },
+  layerButtonContainer: {
+    bottom: 20,
+    right: 20,
+    position: 'absolute',
+  },
+  layerButton: {
+    borderRadius: 12,
+    borderWidth: 2,
+    borderColor: 'gray',
+    backgroundColor: 'white',
   },
   buttonContainer: {
     flexDirection: 'row',
-    marginVertical: 20,
+    top: 20,
+    position: 'absolute',
     backgroundColor: 'transparent',
   },
 });
-
-export default MapsScreen;
