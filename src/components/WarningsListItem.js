b2cf8c81d6af1112464a4efa147d6bdbb2598101
Ilja Leijo <ilja.leijo@fmi.fi>
2020-06-04 13:08:10 +0300

    Add list component and content for warnings

---
+++ src/components/WarningsListItem.js
@@ -0,0 +1,251 @@
+import {
+  View, Text, StyleSheet, TouchableWithoutFeedback,
+} from 'react-native';
+import moment from 'moment-with-locales-es6';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import React from 'react';
+
+const warningsMock = [
+  {
+    time: '20200520',
+    bars: [
+      {
+        color: 'gray',
+        width: '20%'
+      },
+      {
+        color: 'yellow',
+        width: '20%'
+      },
+      {
+        color: 'red',
+        width: '10%'
+      },
+      {
+        color: 'green',
+        width: '50%'
+      },
+    ],
+  },
+  {
+    time: '20200521',
+    bars: [
+      {
+        color: 'yellow',
+        width: '20%'
+      },
+      {
+        color: 'red',
+        width: '10%'
+      },
+      {
+        color: 'green',
+        width: '50%'
+      },
+      {
+        color: 'gray',
+        width: '20%'
+      },
+    ],
+  },
+  {
+    time: '20200522',
+    bars: [
+      {
+        color: 'yellow',
+        width: '20%'
+      },
+      {
+        color: 'red',
+        width: '20%'
+      },
+      {
+        color: 'green',
+        width: '60%'
+      },
+    ],
+  },
+  {
+    time: '20200523',
+    bars: [
+      {
+        color: 'green',
+        width: '40%'
+      },
+      {
+        color: 'red',
+        width: '10%'
+      },
+      {
+        color: 'yellow',
+        width: '50%'
+      },
+    ],
+  },
+  {
+    time: '20200524',
+    bars: [
+      {
+        color: 'red',
+        width: '90%'
+      },
+      {
+        color: 'yellow',
+        width: '5%'
+      },
+      {
+        color: 'green',
+        width: '5%'
+      },
+    ],
+  },
+]
+
+export default class WarningsListItem extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = { isHidden: true }
+  }
+
+  toggleListItem = () => {
+    console.log('toggleListItem')
+    this.setState({ isHidden: !this.state.isHidden })
+  }
+
+  render() {
+
+    const IconComponent = Ionicons;
+    return (
+      <View style={styles.container}>
+        <View style={styles.flatListContainer}>
+          <TouchableWithoutFeedback onPress={this.toggleListItem}>
+            <View style={styles.listItemContainer} >
+              <View style={styles.titleContainer}>
+                <Text style={styles.title}>{this.props.item.item.warningType}</Text>
+              </View>
+
+              <View style={styles.dropButtonContainer}>
+                <View style={styles.dropButton}>
+                  {this.state.isHidden && <IconComponent name='ios-arrow-dropdown-circle' size={25} color={'black'} />}
+                  {!this.state.isHidden && <IconComponent name='ios-arrow-dropup-circle' size={25} color={'black'} />}
+                </View>
+              </View>
+
+
+              <View style={styles.symbolContainer}>
+                <IconComponent name={this.props.item.item.icon} size={35} color={'black'} />
+              </View>
+
+              <View style={styles.warningLoadingContainer}>
+                <View style={styles.weekdayBarContainer}>
+
+                  {
+                    warningsMock.map((element, i) => {
+                      return (
+                        <View key={i} style={styles.warningBarContainer}>
+                          <Text style={styles.warningDayText}>{moment(element.time).format('ddd').toUpperCase()}</Text>
+                          <View style={styles.warningBarColorContainer}>
+                            {
+                              element.bars.map((barElement, k) => {
+                                return (
+                                  <View key={k} style={{ width: barElement.width, height: 6, backgroundColor: barElement.color, }} />
+                                );
+
+                              })
+                            }
+                          </View>
+                        </View>
+                      );
+                    })
+                  }
+                </View>
+              </View>
+            </View>
+          </TouchableWithoutFeedback>
+          {!this.state.isHidden && <View style={styles.collapsableContentContainer}>
+            <Text style={styles.collapsableContentText}>{this.props.item.item.description}</Text>
+          </View>}
+        </View>
+      </View>
+    );
+  }
+}
+
+
+const styles = StyleSheet.create({
+  listItemContainer: {
+    flexDirection: 'row',
+    flex: 1,
+    height: 60,
+    marginTop: 1,
+    marginBottom: 1,
+    backgroundColor: 'white',
+  },
+  titleContainer: {
+    flex: 1,
+    top: 5, left: 0, right: 0, bottom: 0,
+    position: 'absolute',
+    alignItems: 'center',
+  },
+  title: {
+    fontWeight: 'bold',
+    color: 'black',
+  },
+  dropButtonContainer: {
+    top: 0, left: 0, right: 8, bottom: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    alignItems: 'flex-end',
+  },
+  symbolContainer: {
+    top: 0, left: 8, right: 0, bottom: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    alignItems: 'flex-start',
+  },
+  container: {
+    flex: 1,
+    backgroundColor: 'lightgray',
+  },
+  flatListContainer: {
+    flex: 1,
+  },
+  warningLoadingContainer: {
+    top: 8,
+    paddingHorizontal: 45,
+    width: '90%',
+    flexGrow: 1,
+  },
+  warningBarColorContainer: {
+    flexDirection: 'row',
+  },
+  weekdayBarContainer: {
+    flexDirection: 'row',
+    flexGrow: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  warningBarContainer: {
+    flex: 1,
+    paddingHorizontal: 8,
+    textAlign: 'center',
+
+  },
+  warningDayText: {
+    flexDirection: 'row',
+    fontSize: 10,
+    color: 'black',
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+  collapsableContentContainer: {
+    backgroundColor: 'rgb(222,236,246)',
+  },
+  collapsableContentText: {
+    color: 'black',
+    fontSize: 14,
+  }
+});
--- src/screens/WarningsScreen.js
+++ src/screens/WarningsScreen.js
@@ -1,267 +1,111 @@
-import {
-  View, Text, StyleSheet, FlatList, TouchableWithoutFeedback,
-} from 'react-native';
-import moment from 'moment-with-locales-es6';
-import Ionicons from 'react-native-vector-icons/Ionicons';
 import React from 'react';
+import { connect } from 'react-redux';
+import moment from 'moment-with-locales-es6';
+import {
+  View, StyleSheet, FlatList,
+} from 'react-native';
+import { withNavigation } from 'react-navigation';
+import { LoadingView } from '../components';
+import { ErrorView } from '../components';
+import { warningsFetch } from '../actions/WarningsActions';
+import WarningsListItem from '../components/WarningsListItem';
 
 const warningsData = [
   {
     warningType: "Wind",
-    icon: "ios-move"
+    icon: "ios-move",
+    description: 'It is expected strong wind with mean speed between 15 m/s and 18 m/s with higher gusts in Kochkor district. BE AWARE of debris being blown around. Localised outdoor activity disruptions are possible due to debris and dust.',
   },
   {
     warningType: "Flood",
     icon: "ios-water",
+    description: 'Ut ultrices libero imperdiet metus ornare, sit amet elementum felis sagittis. Maecenas in metus facilisis, ullamcorper tellus at, rutrum enim. Curabitur bibendum iaculis rutrum. Integer lacinia metus eget neque fermentum suscipit quis nec mi. Nulla facilisi. Aliquam nec sem dictum felis scelerisque lobortis vitae nec mi. In lectus arcu, ultrices ornare commodo id, maximus malesuada odio. Aliquam nec odio quis dolor eleifend condimentum. Pellentesque vitae fermentum nisi. Nam dignissim fermentum tortor. Ut sit amet purus a nunc egestas ullamcorper.',
   },
   {
     warningType: "Thunderstorm",
     icon: "ios-thunderstorm",
+    description: 'Mauris nec risus dolor. Aliquam neque nisl, vehicula ac feugiat eget, condimentum quis ex. Etiam mattis velit volutpat feugiat ullamcorper. Etiam cursus pharetra elit, nec porta nunc cursus non. Vestibulum bibendum tempus nunc quis porta. Phasellus quis pellentesque velit. Nunc lacinia orci sed turpis sagittis ultrices. Nunc vitae diam nec massa tempor elementum quis viverra urna. Mauris at enim velit. Suspendisse non libero in erat pellentesque iaculis.',
   },
   {
     warningType: "Forest fire",
     icon: "md-bonfire",
+    description: 'Aliquam hendrerit maximus sollicitudin. Fusce ullamcorper semper lacus, vitae lobortis nunc consectetur non. Etiam auctor quis quam id hendrerit. Nunc placerat finibus tellus, vitae condimentum felis pellentesque vitae. Donec pulvinar pharetra tellus, ac aliquam sem congue quis. Praesent faucibus condimentum eros, vitae laoreet ligula viverra at. Duis id lobortis metus. Nunc a pulvinar justo, non vehicula lorem. In dapibus justo eget erat ullamcorper sollicitudin.',
   },
 ]
 
-const warningsMock = [
-  {
-    time: '20200520',
-    bars: [
-      {
-        color: 'gray',
-        width: '20%'
-      },
-      {
-        color: 'yellow',
-        width: '20%'
-      },
-      {
-        color: 'red',
-        width: '10%'
-      },
-      {
-        color: 'green',
-        width: '50%'
-      },
-    ],
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F7F7F7',
   },
-  {
-    time: '20200521',
-    bars: [
-      {
-        color: 'yellow',
-        width: '20%'
-      },
-      {
-        color: 'red',
-        width: '10%'
-      },
-      {
-        color: 'green',
-        width: '50%'
-      },
-      {
-        color: 'gray',
-        width: '20%'
-      },
-    ],
+  flatListContainer: {
+    flex: 1,
+    marginTop: 10,
   },
-  {
-    time: '20200522',
-    bars: [
-      {
-        color: 'yellow',
-        width: '20%'
-      },
-      {
-        color: 'red',
-        width: '20%'
-      },
-      {
-        color: 'green',
-        width: '60%'
-      },
-    ],
-  },
-  {
-    time: '20200523',
-    bars: [
-      {
-        color: 'green',
-        width: '40%'
-      },
-      {
-        color: 'red',
-        width: '10%'
-      },
-      {
-        color: 'yellow',
-        width: '50%'
-      },
-    ],
-  },
-  {
-    time: '20200524',
-    bars: [
-      {
-        color: 'red',
-        width: '90%'
-      },
-      {
-        color: 'yellow',
-        width: '5%'
-      },
-      {
-        color: 'green',
-        width: '5%'
-      },
-    ],
-  },
-]
+});
 
-export default class WarningsScreen extends React.Component {
 
-  constructor(props) {
-    super(props)
-    this.state = { isHidden: true }
+export class WarningsScreen extends React.Component {
+
+  renderLoading() {
+    return (
+      <LoadingView />
+    );
   }
 
-  toggleListItem = () => {
-    console.log('toggleListItem')
-    this.setState({ isHidden: !this.state.isHidden })
+  renderError() {
+    return (
+      <ErrorView />
+    );
   }
 
-  render() {
-    const IconComponent = Ionicons;
+  getListData() {
+    const listData = [];
+
+    return listData;
+  }
+
+  onRefresh() {
+    console.log('refreshed')
+    this.props.tsFetch();
+    this.props.warningsFetch();
+  }
+
+  renderFlatList() {
+
+    if (this.props.navigation.state.params) {
+      if (this.props.navigation.state.params.refreshLocation) {
+        this.onRefresh()
+        this.props.navigation.state.params.refreshLocation = false
+      }
+    }
+
     return (
       <View style={styles.container}>
         <View style={styles.flatListContainer}>
           <FlatList
             style={{ flex: 1 }}
             data={warningsData}
-            renderItem={({ item }) => (
-              <TouchableWithoutFeedback onPress={this.toggleListItem}>
-                <View style={styles.listItemContainer} >
-                  <View style={styles.titleContainer}>
-                    <Text style={styles.title}>{item.warningType}</Text>
-                  </View>
-
-                  <View style={styles.dropButtonContainer}>
-                    <View style={styles.dropButton}>
-                      {this.state.isHidden && <IconComponent name="ios-arrow-dropdown-circle" size={25} color={'black'} />}
-                      {!this.state.isHidden && <IconComponent name="ios-arrow-dropup-circle" size={25} color={'black'} />}
-                    </View>
-                  </View>
-
-
-                  <View style={styles.symbolContainer}>
-                    <IconComponent name={item.icon} size={35} color={'black'} />
-                  </View>
-
-                  <View style={styles.warningLoadingContainer}>
-                    <View style={styles.weekdayBarContainer}>
-
-                      {
-                        warningsMock.map((element, i) => {
-                          return (
-                            <View key={i} style={styles.warningBarContainer}>
-                              <Text style={styles.warningDayText}>{moment(element.time).format('ddd').toUpperCase()}</Text>
-                              <View style={styles.warningBarColorContainer}>
-                                {
-                                  element.bars.map((barElement, k) => {
-                                    return (
-                                      <View key={k} style={{ width: barElement.width, height: 6, backgroundColor: barElement.color, }} />
-                                    );
-
-                                  })
-                                }
-                              </View>
-                            </View>
-                          );
-                        })
-                      }
-                    </View>
-                  </View>
-                </View>
-              </TouchableWithoutFeedback>
-            )}
+            renderItem={(item) => <WarningsListItem item={item} />}
             keyExtractor={(item) => item.warningType}
             scrollEnabled
+            //onRefresh={() => this.onRefresh()}
+            refreshing={this.props.loading}
           />
         </View>
       </View>
     );
   }
+
+  render() {
+    if (this.props.loading) {
+      return this.renderLoading();
+    }
+    if (this.props.error) {
+      return this.renderError();
+    }
+    return this.renderFlatList();
+  }
 }
 
-
-const styles = StyleSheet.create({
-  listItemContainer: {
-    flexDirection: 'row',
-    flex: 1,
-    height: 60,
-    marginTop: 1,
-    marginBottom: 1,
-    backgroundColor: 'white',
-  },
-  titleContainer: {
-    flex: 1,
-    top: 5, left: 0, right: 0, bottom: 0,
-    position: 'absolute',
-    alignItems: 'center',
-  },
-  title: {
-    fontWeight: 'bold',
-    color: 'black',
-  },
-  dropButtonContainer: {
-    top: 0, left: 0, right: 8, bottom: 0,
-    justifyContent: 'center',
-    alignItems: 'center',
-    position: 'absolute',
-    alignItems: 'flex-end',
-  },
-  symbolContainer: {
-    top: 0, left: 8, right: 0, bottom: 0,
-    justifyContent: 'center',
-    alignItems: 'center',
-    position: 'absolute',
-    alignItems: 'flex-start',
-  },
-  container: {
-    flex: 1,
-    backgroundColor: 'lightgray',
-  },
-  flatListContainer: {
-    flex: 1,
-  },
-  warningLoadingContainer: {
-    top: 8,
-    paddingHorizontal: 45,
-    width: '90%',
-    flexGrow: 1,
-  },
-  warningBarColorContainer: {
-    flexDirection: 'row',
-  },
-  weekdayBarContainer: {
-    flexDirection: 'row',
-    flexGrow: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  warningBarContainer: {
-    flex: 1,
-    paddingHorizontal: 8,
-    textAlign: 'center',
-
-  },
-  warningDayText: {
-    flexDirection: 'row',
-    fontSize: 10,
-    color: 'black',
-    textAlign: 'center',
-    fontWeight: 'bold',
-  },
-});
+export default WarningsScreen;
