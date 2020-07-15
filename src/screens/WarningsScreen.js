6456eed927cc9bcfd2d6f13927c4e254fbfc8c3b
Ilja Leijo <ilja.leijo@fmi.fi>
2020-07-15 15:45:34 +0300

    Add pull to refresh for WarningsErrorView and WarningsNotSetView

--- src/screens/WarningsScreen.js
+++ src/screens/WarningsScreen.js
@@ -32,28 +32,25 @@ export class WarningsScreen extends React.Component {
   renderError() {
     const { t } = this.props;
     return (
-      <WarningsErrorView t={t} />
+      <View style={styles.container}>
+        <View style={styles.flatListContainer}>
+          <FlatList
+            style={{ flex: 1 }}
+            ListEmptyComponent={<WarningsErrorView t={t} />}
+            onRefresh={() => this.onRefresh()}
+            refreshing={this.props.warningsLoading}
+          />
+        </View>
+      </View>
     );
-  }
-
-  renderWarningsNotSet() {
-    const { t } = this.props;
-    return (
-      <WarningsNotSetView t={t} />
-    );
-  }
-
-  getListData() {
-    const listData = [];
-
-    return listData;
   }
 
   onRefresh() {
-    this.props.tsFetch().then(() => this.props.warningsFetch());
+    this.props.warningsFetch();
   }
 
   renderFlatList() {
+    const { t } = this.props;
     if (this.props.navigation.state.params) {
       if (this.props.navigation.state.params.refreshLocation) {
         this.onRefresh();
@@ -66,12 +63,13 @@ export class WarningsScreen extends React.Component {
         <View style={styles.flatListContainer}>
           <FlatList
             style={{ flex: 1 }}
-            data={this.props.warningsObj[i18n.language] || this.props.warningsObj.en || this.props.warningsObj[Object.keys(this.props.warningsOb)[0]]}
+            data={this.props.warningsObj[i18n.language] || this.props.warningsObj.en || this.props.warningsObj[Object.keys(this.props.warningsObj)[0]]}
             renderItem={(item) => <WarningsListItem item={item} />}
             keyExtractor={(item) => item.id}
             scrollEnabled
             onRefresh={() => this.onRefresh()}
             refreshing={this.props.warningsLoading}
+            ListEmptyComponent={<WarningsNotSetView t={t} />}
           />
         </View>
       </View>
@@ -84,9 +82,6 @@ export class WarningsScreen extends React.Component {
     }
     if (this.props.warningsError) {
       return this.renderError();
-    }
-    if (_.isPlainObject(this.props.warningsObj) && _.isEmpty(this.props.warningsObj) && !this.props.warningsError) {
-      return this.renderWarningsNotSet();
     }
     return this.renderFlatList();
   }
