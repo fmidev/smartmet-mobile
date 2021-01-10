9ab6d1cee7d0f49b8f81bad1e2003bbb6375cd95
Ilja Leijo <ilja.leijo@fmi.fi>
2021-01-10 22:37:19 +0200

    Add patch to show png symbols in ios

--- src/screens/FeedbackScreen.js
+++ src/screens/FeedbackScreen.js
@@ -16,6 +16,22 @@ const styles = StyleSheet.create({
     fontFamily: 'Roboto-Regular',
     color: 'rgb(48,49,147)',
   },
+  feedbackButton: {
+    alignItems: 'center',
+    marginRight: 40,
+    marginLeft: 40,
+    marginTop: 10,
+    paddingTop: 10,
+    paddingBottom: 10,
+    backgroundColor: 'rgb(48,49,147)',
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: '#fff'
+  },
+  feedbackButtonText: {
+    color: '#fff',
+    fontFamily: 'Roboto-Regular',
+  }
 });
 
 export class FeedbackScreen extends React.Component {
@@ -30,13 +46,13 @@ export class FeedbackScreen extends React.Component {
     return (
       <View style={styles.container}>
         <Text style={styles.contentText}>Use the Send feedback button to give us feedback on the application. The button will take you to your mobile phone's email program. {'\n\n'}{`${t('feedback:content')} `}</Text>
-        <View style={{ flex: 1, paddingTop: 30, width: '80%', }}>
-          <Button
-            title='SEND FEEDBACK'
-            color='rgb(48,49,147)'
-            accessibilityLabel='Tap to Decrypt Data'
+        <View style={{ flex: 1, paddingTop: 30, width: '80%' }}>
+          <TouchableOpacity
+            style={styles.feedbackButton}
             onPress={() => Linking.openURL('mailto:support@example.com?subject=SmartMet Mobile')}
-          />
+            underlayColor='#fff'>
+            <Text style={styles.feedbackButtonText}>SEND FEEDBACK</Text>
+          </TouchableOpacity>
         </View>
       </View>
     );
