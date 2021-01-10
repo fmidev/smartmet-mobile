2f54ae95235a832d310ebb4597ebc0044708b342
Ilja Leijo <ilja.leijo@fmi.fi>
2021-01-10 21:21:08 +0200

    Add safeareaview wrapper for the app

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ import { Provider } from 'react-redux';
 import ReduxThunk from 'redux-thunk';
 import { translate } from 'react-i18next';
 import reducers from './src/reducers';
+import { SafeAreaView } from 'react-navigation';
 import i18n from './src/I18n/index';
 import Stack from './src/navigators/stack-navigator';
 
@@ -22,9 +23,11 @@ export default class App extends React.Component {
   render() {
     const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
     return (
-      <Provider store={store}>
-        <ReloadAppOnLanguageChange />
-      </Provider>
+      <SafeAreaView style={{ flex: 1 }}>
+        <Provider store={store}>
+          <ReloadAppOnLanguageChange />
+        </Provider>
+      </SafeAreaView>
     );
   }
 }
