d4811ac47dffb50abc5c6ab973103905fb8ecc8f
Ilja Leijo <ilja.leijo@fmi.fi>
2021-01-10 14:28:07 +0200

    Add close button and check mark for rb sheet in settings

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -1,22 +1,25 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, FlatList } from 'react-native';
+import {
+  View, Text, StyleSheet, TouchableOpacity, FlatList,
+} from 'react-native';
 import { translate } from 'react-i18next';
 import i18n from 'i18next';
 import { ListItem } from 'react-native-elements';
 import AsyncStorage from '@react-native-community/async-storage';
-import RBSheet from "react-native-raw-bottom-sheet";
+import RBSheet from 'react-native-raw-bottom-sheet';
+import { connect } from 'react-redux';
 import { settingsChange } from '../actions/SettingsActions';
 import { setLang } from '../actions/QueryParamActions';
 import { tsFetchUpdate } from '../actions/TimeSeriesActions';
 import { UNITS } from '../Constants';
-import { connect } from 'react-redux';
-import { asyncStorageSetItem } from '../components/Helper'
-import CheckActiveLightMode from '../assets/images/icons/checkActiveLightMode.svg'
-import ArrowLeft from '../components/ArrowLeft'
+import { asyncStorageSetItem } from '../components/Helper';
+import CheckActiveLightMode from '../assets/images/icons/checkActiveLightMode.svg';
+import ArrowLeft from '../components/ArrowLeft';
+import CloseLightMode from '../assets/images/icons/closeLightMode.svg';
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#FFF'
+    backgroundColor: '#FFF',
   },
   header: {
     backgroundColor: 'rgb(238,244,251)',
@@ -59,28 +62,45 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingRight: 25,
   },
+  rbContainer: {
+    flex: 1,
+    paddingTop: 20,
+  },
   rbTitle: {
+    flexDirection: 'row',
+    paddingLeft: 20,
+    paddingBottom: 10,
+  },
+  rbTitleText: {
     color: 'rgb(48,49,147)',
     fontSize: 16,
     fontFamily: 'Roboto-Medium',
-    paddingTop: 15,
-    paddingBottom: 15,
-    paddingLeft: 10,
     textTransform: 'capitalize',
   },
+  rbCloseButton: {
+    flex: 1,
+    alignItems: 'flex-end',
+    paddingRight: 20,
+  },
   listItemRBSheetTitle: {
+    paddingLeft: 6,
     color: 'rgb(48,49,147)',
     fontSize: 15,
     fontFamily: 'Roboto-Regular',
-    marginLeft: 10,
-  }
+  },
+  listItemDivider: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgb(216,231,242)',
+    height: 1,
+    marginHorizontal: 20,
+  },
 });
 
 export class SettingsScreen extends React.Component {
-
   static navigationOptions = ({ navigation, screenProps }) => ({
     title: screenProps.t('settings:settings'),
-    headerLeft: () => (<TouchableOpacity onPress={() => navigation.goBack(null)} style={{ paddingLeft: 10, }}><ArrowLeft /></TouchableOpacity>)
+    headerLeft: () => (<TouchableOpacity onPress={() => navigation.goBack(null)} style={{ paddingLeft: 10 }}><ArrowLeft /></TouchableOpacity>),
   });
 
   async onChangeLang(lang) {
@@ -95,7 +115,7 @@ export class SettingsScreen extends React.Component {
   }
 
   async onChangeUnit(key, value) {
-    value = value.toString()
+    value = value.toString();
     asyncStorageSetItem(key, value).then(() => {
       this.props.settingsChange(key, value);
     }).catch((error) => {
@@ -122,7 +142,7 @@ export class SettingsScreen extends React.Component {
     const appLanguage = i18n.language;
     return (
 
-      <View style={styles.container} >
+      <View style={styles.container}>
 
         <Text style={styles.header}>
           {t('settings:language')}
@@ -131,74 +151,103 @@ export class SettingsScreen extends React.Component {
         <FlatList
           data={availableLanguages}
           ItemSeparatorComponent={this.renderSeparator}
-          renderItem={({ item }) => {
-            return (
-              <TouchableOpacity onPress={() => this.onChangeLang(item)}>
-                <View style={styles.flatlistItem} >
-                  <Text style={styles.languageListitemText} >{t('settings:' + item)}</Text>
-                  {appLanguage === item ? <View style={styles.listIconRightEnd}><CheckActiveLightMode /></View> : null}
-                </View>
-              </TouchableOpacity>
-            )
-          }}
-          keyExtractor={item => item.toString()}
+          renderItem={({ item }) => (
+            <TouchableOpacity onPress={() => this.onChangeLang(item)}>
+              <View style={styles.flatlistItem}>
+                <Text style={styles.languageListitemText}>{t(`settings:${item}`)}</Text>
+                {appLanguage === item ? <View style={styles.listIconRightEnd}><CheckActiveLightMode /></View> : null}
+              </View>
+            </TouchableOpacity>
+          )}
+          keyExtractor={(item) => item.toString()}
         />
 
-        < Text style={styles.header} >
+        <Text style={styles.header}>
           {t('settings:units')}
-        </Text >
+        </Text>
         <FlatList
           data={UNITS}
           ItemSeparatorComponent={this.renderSeparator}
-          renderItem={({ item }) =>
+          renderItem={({ item }) => (
             <TouchableOpacity onPress={() => this[RBSheet + item.parameterName].open()}>
-              <View style={styles.flatlistItem} >
-                <Text style={styles.settingslistitem} >  {t('settings:' + item.parameterName)} </Text>
-                <Text style={styles.settingslistitemAbb} >{item.parameterName === 'temperature' ? '°' : null}{t('unit abbreviations:' + this.props.parameterUnitAbbMap[item.parameterName])}</Text>
+              <View style={styles.flatlistItem}>
+                <Text style={styles.settingslistitem}>
+                  {' '}
+                  {t(`settings:${item.parameterName}`)}
+                  {' '}
+                </Text>
+                <Text style={styles.settingslistitemAbb}>
+                  {item.parameterName === 'temperature' ? '°' : null}
+                  {t(`unit abbreviations:${this.props.parameterUnitAbbMap[item.parameterName]}`)}
+                </Text>
                 <RBSheet
-                  ref={ref => {
+                  ref={(ref) => {
                     this[RBSheet + item.parameterName] = ref;
                   }}
                   height={300}
                   customStyles={{
                     container: {
                       borderTopLeftRadius: 10,
-                      borderTopRightRadius: 10
-                    }
+                      borderTopRightRadius: 10,
+                    },
                   }}
                 >
-                  <View>
-                    <Text style={styles.rbTitle}>  {t('settings:' + item.parameterName)} </Text>
+                  <View style={styles.rbContainer}>
+
+                    <View style={styles.rbTitle}>
+                      <Text style={styles.rbTitleText}>{t(`settings:${item.parameterName}`)}</Text>
+                      <View style={styles.rbCloseButton}>
+                        <TouchableOpacity onPress={() => this[RBSheet + item.parameterName].close()}><CloseLightMode width={35} /></TouchableOpacity>
+                      </View>
+                    </View>
 
                     {
                       item.unitTypes.map((currentUnitAbb) => (
-                        <ListItem
-                          key={currentUnitAbb.unitAbb}
-                          title={t('unit abbreviations:' + currentUnitAbb.unitAbb)}
-                          titleStyle={styles.listItemRBSheetTitle}
-                          bottomDivider
-                          onPress={() => { this[RBSheet + item.parameterName].close(); this.onChangeUnit(item.parameterName, currentUnitAbb.unitId); }}
-                        />
+                        <View>
+                          <ListItem
+                            containerStyle={{ paddingVertical: 12 }}
+                            key={currentUnitAbb.unitAbb}
+                            leftElement={() => (
+                              <View>
+                                <Text style={styles.listItemRBSheetTitle}>
+                                  {item.parameterName === 'temperature' ? '°' : null}
+                                  {t(`unit abbreviations:${currentUnitAbb.unitAbb}`)}
+                                </Text>
+                              </View>
+                            )}
+                            rightElement={currentUnitAbb.unitAbb === this.props.parameterUnitAbbMap[item.parameterName] ? (
+                              <View style={{
+                                paddingBottom: 0, paddingTop: 0, paddingRight: 8, maxHeight: 10, justifyContent: 'center',
+                              }}
+                              >
+                                <CheckActiveLightMode />
+                              </View>
+                            ) : null}
+                            onPress={() => { this.onChangeUnit(item.parameterName, currentUnitAbb.unitId); }}
+                          />
+                          <View style={styles.listItemDivider} />
+
+                        </View>
                       ))
                     }
                   </View>
                 </RBSheet>
               </View>
             </TouchableOpacity>
-          }
+          )}
           keyExtractor={(item) => item.parameterName}
         />
 
-        < Text style={styles.header} ></Text>
+        <Text style={styles.header} />
 
-      </View >
+      </View>
     );
   }
 }
 
 const mapStateToProps = (state) => {
-  const { parameterUnitMap } = state.parameterUnitMap
-  const { parameterUnitAbbMap } = state.parameterUnitAbbMap
+  const { parameterUnitMap } = state.parameterUnitMap;
+  const { parameterUnitAbbMap } = state.parameterUnitAbbMap;
   return { parameterUnitMap, parameterUnitAbbMap };
 };
 
